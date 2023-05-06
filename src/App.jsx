19720851247a03df8ccc466e911a53d282cab340
Antony Chacko <antony101tony@gmail.com>
2023-05-06 10:01:15 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import ZipUpload from './components/Uploader'
 import Downloader from './components/Downloader'
 import { useState } from 'react'
 
-function App () {
-  const [jsonData, setJsonData] = useState([])
+function App() {
+  const [stringTerms, setStringTerms] = useState([])
+  const [language, setLanguage] = useState("")
 
   const onUpload = file => {
     const new_zip = new JSZip()
@@ -15,24 +16,35 @@ function App () {
 
       const ourData = JSON.parse(jsonFile)
       const stringTerms = ourData.terms
+      setStringTerms(stringTerms)
 
-      const mapped = stringTerms.map(
-        term => `${term.slice(0, 3)}\t${term}\ten-US`
-      )
-      const reversed = mapped.reverse()
-      reversed.push('# Gboard Dictionary version:1')
-      const final = reversed.reverse()
-      setJsonData(final)
     })
   }
-
+  const mapped = stringTerms.map(
+    term => `${term.slice(0, 3)}\t${term}\t${language}`
+  )
+  const reversed = mapped.reverse()
+  reversed.push('# Gboard Dictionary version:1')
+  const final = reversed.reverse()
   return (
     <>
       <div>
         <ZipUpload onUpload={onUpload}></ZipUpload>
       </div>
       <div>
-        <Downloader list={jsonData}></Downloader>
+        <label>Choose the language:</label>
+        <select onChange={(e) => {
+          console.log("e", e);
+          setLanguage(e.target.value)
+        }}
+        >
+          <option value="">All Language</option>
+          <option value="en-IN">Indian English "en-IN"</option>
+          <option value="en-US">US English "en-US"</option>
+        </select>
+      </div>
+      <div>
+        <Downloader list={final}></Downloader>
       </div>
     </>
   )
